60feae42af639cc30ceebf2f9b1abd72de158a57
Andrew <bewerlyproduction@gmail.com>
2025-05-27 19:35:23 +0300

    Updated DI

--- lib/core/dependency_injection/dependency_injection.dart
+++ lib/core/dependency_injection/dependency_injection.dart
@@ -1,30 +1,72 @@
-import 'package:ai_chat_bot/domain/repositories/impl_llm_repository.dart';
+import 'package:ai_chat_bot/data/repositories/impl_llm_repository.dart';
 import 'package:ai_chat_bot/presentation/bloc/chat_bloc.dart';
+import 'package:ai_chat_bot/presentation/bloc/history_bloc.dart';
+import 'package:ai_chat_bot/presentation/bloc/home_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:ai_chat_bot/data/services/gemini_service.dart';
+import 'package:ai_chat_bot/data/services/hive_storage_service.dart';
+import 'package:ai_chat_bot/data/repositories/impl_chat_history_repository.dart';
 import 'package:ai_chat_bot/domain/repositories/llm_repository.dart';
-import 'package:ai_chat_bot/domain/usecases/generate_text_usecase.dart';
+import 'package:ai_chat_bot/domain/repositories/chat_history_repository.dart';
+import 'package:ai_chat_bot/domain/usecases/generate_text_with_context_usecase.dart';
+import 'package:ai_chat_bot/domain/usecases/save_chat_session_usecase.dart';
+import 'package:ai_chat_bot/domain/usecases/get_chat_sessions_usecase.dart';
+import 'package:ai_chat_bot/domain/usecases/get_chat_session_usecase.dart';
+import 'package:ai_chat_bot/domain/usecases/delete_chat_session_usecase.dart';
 
 final GetIt sl = GetIt.instance;
 
 void init() {
-  // Services
+  // Storage Services
+  sl.registerLazySingleton<HiveStorageService>(
+    () => HiveStorageService(),
+  );
+
+  // API Services
   sl.registerLazySingleton<GeminiService>(
     () => GeminiService(),
   );
 
-  //Repositories
+  // Repositories
   sl.registerLazySingleton<LLMRepository>(
     () => ImplLLMRepository(sl.get()),
   );
 
+  sl.registerLazySingleton<ChatHistoryRepository>(
+    () => ImplChatHistoryRepository(sl.get()),
+  );
+
   // Use cases
-  sl.registerLazySingleton<GenerateTextUseCase>(
-    () => GenerateTextUseCase(sl.get()),
+  sl.registerLazySingleton<GenerateTextWithContextUseCase>(
+    () => GenerateTextWithContextUseCase(sl.get()),
+  );
+
+  sl.registerLazySingleton<SaveChatSessionUseCase>(
+    () => SaveChatSessionUseCase(sl.get()),
+  );
+
+  sl.registerLazySingleton<GetChatSessionsUseCase>(
+    () => GetChatSessionsUseCase(sl.get()),
+  );
+
+  sl.registerLazySingleton<GetChatSessionUseCase>(
+    () => GetChatSessionUseCase(sl.get()),
+  );
+
+  sl.registerLazySingleton<DeleteChatSessionUseCase>(
+    () => DeleteChatSessionUseCase(sl.get()),
   );
 
   // Blocs
   sl.registerFactory<ChatBloc>(
-    () => ChatBloc(sl.get()),
+    () => ChatBloc(sl.get(), sl.get(), sl.get()),
+  );
+
+  sl.registerFactory<HistoryBloc>(
+    () => HistoryBloc(sl.get(), sl.get()),
+  );
+
+  sl.registerFactory<HomeBloc>(
+    () => HomeBloc(sl.get()),
   );
 }
